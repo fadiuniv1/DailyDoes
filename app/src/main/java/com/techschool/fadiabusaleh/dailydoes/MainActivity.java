7acb11943ba26a5c90cac21836fd87a2e487b0b1
Fadi Abu Saleh <fadiuniv1@gmail.com>
2018-11-29 18:32:29 +0200

    FireBase Successful connection

--- app/src/main/java/com/techschool/fadiabusaleh/dailydoes/MainActivity.java
+++ app/src/main/java/com/techschool/fadiabusaleh/dailydoes/MainActivity.java
@@ -3,11 +3,17 @@ package com.techschool.fadiabusaleh.dailydoes;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends Activity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myRef.setValue("Hello, World!");
     }
 }
